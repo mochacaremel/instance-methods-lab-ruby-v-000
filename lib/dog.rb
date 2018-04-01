23b74c7aa3c27ad18157487457c215d5f7193cc9
Asheley McDonald <asheleymcdonald@gmail.com>
2018-04-01 21:40:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,10 @@
 class Dog
-  # Class body
-
-  # Instance Method Definition
   def bark
     puts "Woof!"
   end
 end
 
-# Instance Method Definition
 def sit
   puts "The Dog is sitting"
-  end
+end
+end
